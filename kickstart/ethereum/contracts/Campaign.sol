59cda6f4247552d726bfa9f7927a0cc9dd7fb649
subu-nair1798 <subunair17@gmail.com>
2020-07-02 15:56:25 +0530

    kickstart compile script

---
+++ kickstart/ethereum/contracts/Campaign.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.5.0 <0.7.0;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = address(new Campaign(minimum, msg.sender));
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[] memory) {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address payable recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+
+    modifier restricted() {
+        require(msg.sender == manager, "A minimum contribution is required");
+        _;
+    }
+
+    constructor (uint minimum, address creator) public {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution, "Value should be greater than Minimum contribution");
+
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+
+    function createRequest(string memory description, uint value, address payable recipient) public restricted {
+        require(approvers[msg.sender], "Only the manager can create a request");
+
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+
+        requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender], "Only the contributors can approve a request");
+        require(!request.approvals[msg.sender], "You can only approve the request once");
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+
+        require(request.approvalCount > (approversCount / 2), "The number of approvers should be greater than 50%");
+        require(!request.complete, "The request has already been finalized");
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+
+}
